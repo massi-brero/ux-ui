75e2372d4ac6cb71441b8fa89a5ea746a4bffec7
Brero, Massimiliano <massimiliano@adess.de>
2020-05-20 00:08:34 +0200

    first commit

---
+++ projects/pwa_1/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/images/logo.png"]);
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(res => {
+        return res || fetch(event.request);
+    }))
+});
